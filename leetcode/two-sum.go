716a890874683ee3c789e306b656ff4d1271cf2c
SangKil Park <ht1911@gmail.com>
2020-02-20 12:48:10 +0900

    two-sum

---
+++ leetcode/two-sum.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	index := make(map[int]int, len(nums))
+
+	for i, b := range nums {
+		if j, ok := index[target-b]; ok {
+			return []int{j, i}
+		}
+		index[b] = i
+	}
+
+	return nil
+}
