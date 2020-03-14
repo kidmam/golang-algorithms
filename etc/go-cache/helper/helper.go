5e6715fe8399f78cb3cec9b375bfcf618b9861e9
SangKil Park <ht1911@gmail.com>
2020-03-14 17:51:19 +0900

    How To Set and Get Cache Using Golang Rest Api

---
+++ etc/go-cache/helper/helper.go
@@ -0,0 +1,28 @@
+package helper
+
+import (
+	"github.com/patrickmn/go-cache"
+	"time"
+)
+
+var Cache = cache.New(5*time.Minute, 5*time.Minute)
+
+type Emp []struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func SetCache(key string, emp interface{}) bool {
+	Cache.Set(key, emp, cache.NoExpiration)
+	return true
+}
+
+func GetCache(key string) (Emp, bool) {
+	var emp Emp
+	var found bool
+	data, found := Cache.Get(key)
+	if found {
+		emp = data.(Emp)
+	}
+	return emp, found
+}
