5eab0f85a1e955cae5f0af50d9a64a95152ba03c
SangKil Park <ht1911@gmail.com>
2020-03-17 22:25:49 +0900

    Keep Interfaces Small

--- etc/keep_Interfaces_small/keep_Interfaces_small.go
+++ etc/keep_Interfaces_small/keep_Interfaces_small.go
@@ -5,10 +5,22 @@ import (
 	"os"
 )
 
+// https://dev.to/wagslane/best-practices-for-writing-clean-interfaces-in-go-5c2j?utm_source=dormosheio&utm_campaign=dormosheio
 type File interface {
 	io.Closer
 	io.Reader
 	io.Seeker
 	Readdir(count int) ([]os.FileInfo, error)
 	Stat() (os.FileInfo, error)
+}
+
+type car interface {
+	GetColor() string
+	GetSpeed() int
+	IsFiretruck() bool
+}
+
+type firetruck interface {
+	car
+	HoseLength() int
 }
