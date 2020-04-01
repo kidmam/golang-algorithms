89c93a5236ce241380a9788d09a353741b9e47d3
SangKil Park <ht1911@gmail.com>
2020-04-01 13:10:09 +0900

    write benchmark

---
+++ training/write-benchmark/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+// go test -v -bench=. -run=none -benchmem .
+import "testing"
+
+func TestPrintInt2String01(t *testing.T) {
+	if printInt2String01(100) != "100" {
+		t.Fatal("error")
+	}
+}
+
+func TestPrintInt2String02(t *testing.T) {
+	if printInt2String02(int64(100)) != "100" {
+		t.Fatal("error")
+	}
+}
+
+func TestPrintInt2String03(t *testing.T) {
+	if printInt2String03(100) != "100" {
+		t.Fatal("error")
+	}
+}
+
+func BenchmarkPrintInt2String01(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		printInt2String01(100)
+	}
+}
+
+func BenchmarkPrintInt2String02(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		printInt2String02(int64(100))
+	}
+}
+
+func BenchmarkPrintInt2String03(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		printInt2String03(100)
+	}
+}
